04ebfbae2ada6bff033e554305583152031a20df
hirak <hirakmandal81@gmail.com>
2026-05-13 18:42:05 +0530

    FastAPI app
    A centralized backend system command center and the gateway of the system.

--- 04_rag_agent_development/backend/main.py
+++ 04_rag_agent_development/backend/main.py
@@ -1,19 +1,10 @@
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-#creating FastAPI application/server instance
+from api.chat import router
+
+#create main FastAPI application/server instance
 app=FastAPI()
 
-#request validation 
-class ChatRequest(BaseModel):
-    message: str
-    user_id: str
-    session_id: str
-    conversation_id: Optional[str]=None
+#connect routes to app
+app.include_router(router)
 
-#sending request to backend through POST method
-@app.post("/chat")
-def logic(request: ChatRequest):
-    user_message=request.message
-    return f"I've received your message:{user_message}"
     
