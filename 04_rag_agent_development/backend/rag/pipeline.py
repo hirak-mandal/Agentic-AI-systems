6f86d375518865531ed1c2cb2494929ad960293e
hirak <hirakmandal81@gmail.com>
2026-05-16 15:14:44 +0530

    RAG pipeline complete

--- 04_rag_agent_development/backend/rag/pipeline.py
+++ 04_rag_agent_development/backend/rag/pipeline.py
@@ -1,7 +1,9 @@
 from utils.file_loader import document_loader
 from rag.text_chunker import chunk_documents
 from rag.embedder import embed_chunks
+from rag.faiss_store import store_embeddings
 
 documents=document_loader()
 chunks=chunk_documents(documents)
 embeddings=embed_chunks(chunks)
+faiss_index=store_embeddings(embeddings)
