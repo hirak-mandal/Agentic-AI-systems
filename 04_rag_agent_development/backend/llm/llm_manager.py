4a69f26c3677b134f0c4941128d943fe37fa7a52
hirak <hirakmandal81@gmail.com>
2026-05-17 16:48:42 +0530

    fully completed

--- 04_rag_agent_development/backend/llm/llm_manager.py
+++ 04_rag_agent_development/backend/llm/llm_manager.py
@@ -1,8 +1,13 @@
 from llm.gemini_client import client
 from core.prompts import system_prompt
+from rag.retriever import retrieve_chunks
+from rag.pipeline import chunks
+
 def llm_response(query):
+    #searched chunks which are similar to user query
+    similar_chunks=retrieve_chunks(query,chunks)
     response=client.models.generate_content(
-        model="gemini-1.5-flash",
-        contents=system_prompt + query
+        model="gemini-2.5-flash",
+        contents=system_prompt + "\nUser query" + query + "\ncontext\n" + "\n".join(similar_chunks)
     )
     return response.text
